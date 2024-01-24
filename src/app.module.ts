ca3d1ca9a255d3c80d12a01f11aeb1b8fc7fcfe4
norieau <a.arijogja@gmail.com>
2024-01-24 23:00:28 +0700

    configurating MySQL and integrating TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductsController } from './products/products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'norieau',
+    password: 'ganteng',
+    database: 'online_store',
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    synchronize: true
+  })],
   controllers: [AppController, ProductsController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }

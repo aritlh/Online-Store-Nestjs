c2f2dc7910b98a231754e15eb8bb5b6d05bacb1b
arijkt <alharitsfaqih@gmail.com>
2024-01-24 12:36:52 +0700

    feat: add index and about pages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,9 +3,26 @@ import { Controller, Get, Render } from '@nestjs/common';
 @Controller()
 export class AppController {
 
-  @Get('/')
+  @Get()
   @Render('index')
-  root() {
-    return { message: 'Hello world' }
+  index() {
+    return {
+      title: 'Home Page - Online Store'
+    }
+  }
+
+  @Get('/about')
+  @Render('about')
+  about() {
+    let viewData = []
+    viewData['description'] = 'This is an about page ...'
+    viewData['author'] = 'Developed by: norieau'
+
+    let titlePage = 'About us - Online Store'
+    return {
+      title: titlePage,
+      subtitle: 'About us',
+      viewData: viewData
+    }
   }
 }

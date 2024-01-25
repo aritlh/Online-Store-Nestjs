a595c585916e8b313f3fa47829dbaafec5346728
arijkt <alharitsfaqih@gmail.com>
2024-01-25 12:17:03 +0700

    modify entities path

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductsController } from './products/products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-<<<<<<< HEAD
 import { ProductsService } from './products/products.service';
 import { Product } from './models/product.entity';
 
@@ -20,20 +19,6 @@ import { Product } from './models/product.entity';
     }),
     TypeOrmModule.forFeature([Product])
   ],
-=======
-
-@Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'norieau',
-    password: 'ganteng',
-    database: 'online_store',
-    entities: ['dist/**/*.entity{.ts,.js}'],
-    synchronize: true
-  })],
->>>>>>> master
   controllers: [AppController, ProductsController],
   providers: [ProductsService],
 })

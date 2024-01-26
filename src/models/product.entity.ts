7d360eeddb1e03d60d548fd385bffc52b228c419
norieau <a.arijogja@gmail.com>
2024-01-26 22:22:07 +0700

    refactoring list products

--- src/models/product.entity.ts
+++ src/models/product.entity.ts
@@ -25,4 +25,51 @@ export class Product {
 
     @Column()
     price: number
+
+    /**
+     * For each Product attribute, we define its corresponding getter and setter. 
+     * We will use getters and setters to access and modify our entity/model attributes. It has a lot of advantages which we will talk about later.
+     * 
+     * For now, the application looks the same. We only modified the way we access entity attributes. 
+     * So, what is the advantage? Let’s revisit the boss requirement where we need to display all products’ names in uppercase over the entire application.
+     */
+    getId(): number {
+        return this.id
+    }
+
+    setId(id: number) {
+        this.id = id
+    }
+
+    getName(): string {
+        return this.name
+    }
+
+    setName(name: string) {
+        this.name = name
+    }
+
+    getDescription(): string {
+        return this.description
+    }
+
+    setDescription(description: string) {
+        this.description = description
+    }
+
+    getImage(): string {
+        return this.image
+    }
+
+    setImage(image: string) {
+        this.image = image
+    }
+
+    getPrice(): number {
+        return this.price
+    }
+
+    setPrice(price: number) {
+        this.price = price
+    }
 }
